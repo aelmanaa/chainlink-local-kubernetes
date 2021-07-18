add7f9c0100994b976d09a0a098e171c87aa682c
Myamoto <aelmanaa@gmail.com>
2021-07-18 15:04:55 +0200

    Refactor. Split project in 2 main use cases basic-request-model and decentralized-data-model

--- migrations/1_deploy.js
+++ migrations/1_deploy.js
@@ -3,10 +3,13 @@
 const { LinkToken } = require('@chainlink/contracts/truffle/v0.4/LinkToken')
 const { Operator } = require('@chainlink/contracts/truffle/v0.7/Operator')
 const Consumer = artifacts.require('Consumer')
+const conf = require('../config/addr.json')
 const fs = require('fs')
 
 
+
 module.exports = async (deployer, network, [owner]) => {
+
   // Local (development) networks need their own deployment of the LINK
   // token and the Operator contract
   LinkToken.setProvider(deployer.provider)
@@ -16,19 +19,25 @@ module.exports = async (deployer, network, [owner]) => {
     await deployer.deploy(Operator, LinkToken.address, owner, { from: owner })
     await deployer.deploy(Consumer, LinkToken.address, { from: owner })
 
-    let addr = {
+    const addr = {
+      /*
       network: {
         host: deployer.networks[network].host,
         port: deployer.networks[network].port
       },
+      */
       linkTokenAddress: LinkToken.address,
       operatorAddress: Operator.address,
       consumerAddress: Consumer.address
     }
+
     console.log(`Link address is ${LinkToken.address}`)
     console.log(`Operator address is ${Operator.address}`)
     console.log(`Consumer address is ${Consumer.address}`)
-    fs.writeFileSync(__dirname + '/../config/addr.json', JSON.stringify(addr, null, '\t'), 'utf-8')
+
+    const newConf = conf
+    newConf[network] = addr
+    fs.writeFileSync(__dirname + '/../config/addr.json', JSON.stringify(newConf, null, '\t'), 'utf-8')
 
   } catch (err) {
     console.error(err)
--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,38 @@
 
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+require('dotenv').config()
+
+const infuraKey = process.env.INFURA_API_KEY || ""
+const mnemonicPhrase = process.env.MNEMONIC || ""
+
 module.exports = {
   networks: {
     ganache: {
       host: '127.0.0.1',
       port: 8545,
       network_id: '*',
+    },
+    kovan: {
+      provider: () => new HDWalletProvider({
+        mnemonic: {
+          phrase: mnemonicPhrase
+        },
+        providerOrUrl: `wss://kovan.infura.io/ws/v3/${infuraKey}`
+      }),
+      network_id: 42,     // Kovan's id
+      gas: 6300000
+
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider({
+        mnemonic: {
+          phrase: mnemonicPhrase
+        },
+        providerOrUrl: `wss://rinkeby.infura.io/ws/v3/${infuraKey}`
+      }),
+      network_id: 4,     // Rinkeby's id
+      gas: 6300000
+
     }
   },
   compilers: {
